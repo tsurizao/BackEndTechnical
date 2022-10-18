910ed29075cc3eba42de2f9a3bbc48ecce7cbdf0
Chase Martinez <chasemartinez22@gmail.com>
2022-10-18 17:11:49 -0500

    Removed unused imports

--- src/main/java/CrudCreate.java
+++ src/main/java/CrudCreate.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
